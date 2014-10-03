1ab813d77089c92a70bd7e93fd83516011b3ffc7
Lindsey DiLoreto <lindsey@doublesecretagency.com>
2014-10-03 01:03:15 -0700

    (v1.0.0) Proper documentation

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
 Auto-Assign User Group plugin for Craft CMS
 =================================================
 
+##The Hypothetical
+
+Imagine this scenario...
+
+You are the super-admin of a website. On that site, you'll need a small group of business-level admins, and an unlimited amount of ordinary members.
+
+ - Super-admin (you)
+ - Admins
+ - Members
+
+Members cannot register themselves... Their accounts must be created by Admins.
+
+Admins can create new users, **but should not be able to edit the individual permissions of each member**. This is a bit of a problem, since Craft allows you to edit the _individual permissions_ of a user from the exact same page as you would _assign them to a specific user group_.
+
+##The Solution?
+
+This plugin will allow you (the Super-admin) to pre-determine which group a newly registered user will belong to. So in the case of our example, a newly registered user would be automatically assigned to the "Members" group. What's important about this, **you can now prevent Admins from managing user permissions at all**. They no longer need the ability to place a new user into the Members group, so they will no longer have access to _any_ of the custom permissions options.
+
+##Using the Plugin
+
+Once you've installed the plugin, visit:
+
+- **Settings > Plugins > Auto-Assign User Group**
+
+There you will see a list of available user groups, under the heading "User Group(s) to be Auto-Assigned". Simply check every group that a new user should be automatically assigned to, and hit Save. It's just that easy!
+
+If no user groups have been created yet, you'll see a message that "No user groups exist" and a link to "Create one now..."
+
+##Important Note
+
+The user group(s) selected will be assigned to a new user when the new user is created **through the Control Panel**. If your website allows users to register through a front-end form, their group will be automatically assigned in a different way.
+
+If you'd like to auto-assign a user group for anyone who registers via the front-end form, that can be specified on the **Settings > Users** page, below the "Allow public registration?" checkbox.
--- autoassignusergroup/AutoAssignUserGroupPlugin.php
+++ autoassignusergroup/AutoAssignUserGroupPlugin.php
@@ -4,79 +4,80 @@ namespace Craft;
 class AutoAssignUserGroupPlugin extends BasePlugin
 {
 
-    public function init() {
-        parent::init();
-        craft()->on('users.saveUser', function(Event $event) {
-            // Set target group
-            $targetGroups = craft()->plugins->getPlugin('autoAssignUserGroup')->getSettings()->userGroups;
-            // If new user
-            if ($event->params['isNewUser']) {
-                // If "groups" is in POST
-                if (isset($_POST['groups'])) {
-                    // If not an array, it's an empty string
-                    if (!is_array($_POST['groups'])) {
-                        // Set it to an array with the user group ID
-                        $_POST['groups'] = $targetGroups;
-                    } else {
-                        // Merge in the user group ID with any existing ones.
-                        $_POST['groups'] = array_merge($_POST['groups'], $targetGroups);
-                    }
-                }
-            }
-        });
-    }
+	public function init() {
+		parent::init();
+		craft()->on('users.saveUser', function(Event $event) {
+			// Set target group
+			$targetGroups = craft()->plugins->getPlugin('autoAssignUserGroup')->getSettings()->userGroups;
+			// If new user
+			if ($event->params['isNewUser']) {
+				// If "groups" is in POST
+				if (isset($_POST['groups'])) {
+					// If not an array, it's an empty string
+					if (!is_array($_POST['groups'])) {
+						// Set it to an array with the user group ID
+						$_POST['groups'] = $targetGroups;
+					} else {
+						// Merge in the user group ID with any existing ones.
+						$_POST['groups'] = array_merge($_POST['groups'], $targetGroups);
+					}
+				}
+			}
+		});
+	}
 
-    public function getName()
-    {
-        return 'Auto-Assign User Group';
-    }
+	public function getName()
+	{
+		return 'Auto-Assign User Group';
+	}
 
-    public function getVersion()
-    {
-        return '0.9.0';
-    }
+	public function getVersion()
+	{
+		return '1.0.0';
+	}
 
-    public function getDeveloper()
-    {
-        return 'Double Secret Agency';
-    }
+	public function getDeveloper()
+	{
+		return 'Double Secret Agency';
+	}
 
-    public function getDeveloperUrl()
-    {
-        return 'https://github.com/lindseydiloreto/craft-autoassignusergroup';
-        //return 'http://doublesecretagency.com';
-    }
+	public function getDeveloperUrl()
+	{
+		return 'https://github.com/lindseydiloreto/craft-autoassignusergroup';
+		//return 'http://doublesecretagency.com';
+	}
 
-    protected function defineSettings()
-    {
-        return array(
-            'userGroups' => array(AttributeType::Mixed),
-        );
-    }
+	protected function defineSettings()
+	{
+		return array(
+			'userGroups' => array(AttributeType::Mixed),
+		);
+	}
 
-    public function getSettingsHtml()
-    {
-        $options = array();
-        $userGroups = craft()->userGroups->getAllGroups();
-        foreach ($userGroups as $group) {
-            $options[] = array(
-                'label' => $group->name,
-                'value' => $group->id,
-            );
-        }
+	public function getSettingsHtml()
+	{
+		$options = array();
+		$userGroups = craft()->userGroups->getAllGroups();
+		foreach ($userGroups as $group) {
+			$options[] = array(
+				'label' => $group->name,
+				'value' => $group->id,
+			);
+		}
 
-        $checkboxes = craft()->templates->render('_includes/forms/checkboxGroup', array(
-            'name'    => 'userGroups',
-            'options' => $options,
-            'values'  => $this->getSettings()->userGroups,
-        ));
+		$checkboxes = craft()->templates->render('_includes/forms/checkboxGroup', array(
+			'name'    => 'userGroups',
+			'options' => $options,
+			'values'  => $this->getSettings()->userGroups,
+		));
 
-        $noGroups = '<p class="error">No user groups exist. <a href="'.UrlHelper::getCpUrl('settings/users/groups/new').'">Create one now...</a></p>';
+		$noGroups = '<p class="error">No user groups exist. <a href="'.UrlHelper::getCpUrl('settings/users/groups/new').'">Create one now...</a></p>';
 
-        return craft()->templates->render('autoassignusergroup/_settings', array(
-            //'settings' => ,
-            'userGroupsField' => TemplateHelper::getRaw(count($userGroups) ? $checkboxes : $noGroups),
-        ));
-    }
-    
+        craft()->templates->includeCssResource('autoassignusergroup/css/settings.css');
+        
+		return craft()->templates->render('autoassignusergroup/_settings', array(
+			'userGroupsField' => TemplateHelper::getRaw(count($userGroups) ? $checkboxes : $noGroups),
+		));
+	}
+	
 }
